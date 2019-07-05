8564fa75a811d5f1efa7a3126e86af112237aa0b
Jacob Maughan <jacobm.2n527@gmail.com>
2019-07-05 12:45:47 -0400

    created page slider

---
+++ CIT_261/Final/pageSlider.js
@@ -0,0 +1,35 @@
+let sliderScreens = document.querySelectorAll('.slide');
+
+// Clear all screens
+function reset() {
+    for(let i = 0; i < sliderScreens.length; i++){
+        sliderScreens[i].style.display = 'none';
+    }
+}
+
+// Init the slider
+function startSlide() {
+    reset();
+    sliderScreens[0].style.display = 'block';
+}
+
+// Show first slide
+function restart() {
+    reset();
+    sliderScreens[0].style.display = 'block';
+}
+
+// Show next
+function slideRight(){
+    reset();
+    sliderScreens[1].style.display = 'block';
+    setTimeout(slideFinal, 2500);
+}
+
+function slideFinal(){
+    reset();
+    sliderScreens[2].style.display = 'block';
+}
+
+startSlide();
+
